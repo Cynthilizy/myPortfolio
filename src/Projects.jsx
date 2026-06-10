78ef38cb3a74647d8f3675f5fc8a11d4becdc709
Cynthilizy <cynthilizy@gmail.com>
2026-06-10 21:36:19 +0300

    fix-responsiveness

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -33,65 +33,74 @@ function Projects({ theme }) {
   return (
     <div className="projects-container" style={{ color: theme.textPrimary }}>
       <div className="projects-container-inner">
-        <div className="mini-game-projects">
-          <h3 className="project-type">Games</h3>
-          <div className="projects mini-game">
-            {ProjectsList.filter((p) => p.type === "mini-game").map((proj) => (
-              <div id={proj.id} key={proj.id}>
-                <CardFull
-                  image={proj.image}
-                  title={proj.title}
-                  type={proj.type}
-                  linkGit={proj.github}
-                  linkLive={proj.live}
-                  tag={proj.tags}
-                  description={proj.description}
-                  demoType={proj.demoType}
-                  theme={theme}
-                />
-              </div>
-            ))}
+        <div className="all-projects">
+          <div className="mini-game-projects">
+            <h3 className="project-type">Games</h3>
+            <div className="projects mini-game">
+              {ProjectsList.filter((p) => p.type === "mini-game").map(
+                (proj) => (
+                  <div id={proj.id} key={proj.id}>
+                    <CardFull
+                      image={proj.image}
+                      title={proj.title}
+                      type={proj.type}
+                      linkGit={proj.github}
+                      linkLive={proj.live}
+                      tag={proj.tags}
+                      description={proj.description}
+                      demoType={proj.demoType}
+                      theme={theme}
+                    />
+                  </div>
+                ),
+              )}
+            </div>
           </div>
-        </div>
-      </div>
-      <div className="case-study-projects">
-        <h3 className="project-type">Case Study Projects</h3>
-        <div className="projects case-study">
-          {ProjectsList.filter((p) => p.type === "case-study").map((proj) => (
-            <div id={proj.id} key={proj.id}>
-              <CardFull
-                image={proj.image}
-                title={proj.title}
-                type={proj.type}
-                linkGit={proj.github}
-                linkLive={proj.live}
-                tag={proj.tags}
-                description={proj.description}
-                demoType={proj.demoType}
-                theme={theme}
-              />
+
+          <div className="case-study-projects">
+            <h3 className="project-type">Case Study Projects</h3>
+            <div className="projects case-study">
+              {ProjectsList.filter((p) => p.type === "case-study").map(
+                (proj) => (
+                  <div id={proj.id} key={proj.id}>
+                    <CardFull
+                      image={proj.image}
+                      title={proj.title}
+                      type={proj.type}
+                      linkGit={proj.github}
+                      linkLive={proj.live}
+                      tag={proj.tags}
+                      description={proj.description}
+                      demoType={proj.demoType}
+                      theme={theme}
+                    />
+                  </div>
+                ),
+              )}
             </div>
-          ))}
-        </div>
-      </div>
-      <div className="main-projects">
-        <h3 className="project-type">Main Projects</h3>
-        <div className="projects main-project">
-          {ProjectsList.filter((p) => p.type === "main-project").map((proj) => (
-            <div id={proj.id} key={proj.id}>
-              <CardFull
-                image={proj.image}
-                title={proj.title}
-                type={proj.type}
-                linkGit={proj.github}
-                linkLive={proj.live}
-                tag={proj.tags}
-                description={proj.description}
-                demoType={proj.demoType}
-                theme={theme}
-              />
+          </div>
+          <div className="main-projects">
+            <h3 className="project-type">Main Projects</h3>
+            <div className="projects main-project">
+              {ProjectsList.filter((p) => p.type === "main-project").map(
+                (proj) => (
+                  <div id={proj.id} key={proj.id}>
+                    <CardFull
+                      image={proj.image}
+                      title={proj.title}
+                      type={proj.type}
+                      linkGit={proj.github}
+                      linkLive={proj.live}
+                      tag={proj.tags}
+                      description={proj.description}
+                      demoType={proj.demoType}
+                      theme={theme}
+                    />
+                  </div>
+                ),
+              )}
             </div>
-          ))}
+          </div>
         </div>
       </div>
     </div>
